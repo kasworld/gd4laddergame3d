c10e352a063f1d8b266381bff266a4567b89018d
SeukWon Kang <kasworld@gmail.com>
2025-02-22 10:34:39 +0900

    사다리 문제 만들기,풀기

--- main.gd
+++ main.gd
@@ -25,6 +25,60 @@ func 사다리칸수() -> Vector2i:
 	var n = 참가자들.get_child_count()
 	return Vector2i(n, n*3 )
 
+func 사다리자료_보기():
+	var 칸수 = 사다리칸수()
+	for y in 칸수.y:
+		var ss :String = ""
+		for x in 칸수.x:
+			ss += str(사다리자료[x][y]) + " "
+		print(ss)
+	for i in 칸수.x:
+		print(i, "->", 참가자위치[i])
+
+func 사다리자료_만들기() -> void:
+	# 초기화
+	var 칸수 = 사다리칸수()
+	풀이이동좌표 = []
+	참가자위치 = []
+	사다리자료 = []
+	for i in 칸수.x:
+		참가자위치.append(i)
+		풀이이동좌표.append([])
+		사다리자료.append([])
+		for j in 칸수.y:
+			사다리자료[i].append(사다리구성자료.new())
+
+	# 문제 만들기
+	for y in 칸수.y:
+		for x in 칸수.x:
+			if randf() < 0.5:
+				continue
+			if 사다리자료[(x-1+칸수.x)%칸수.x][y].왼쪽연결길 == true or 사다리자료[(x+1)%칸수.x][y].왼쪽연결길 == true:
+				continue
+			if y > 0 and 사다리자료[x][y-1].왼쪽연결길 == true:
+				continue
+			사다리자료[x][y].왼쪽연결길 = true
+
+	# 풀이 만들기
+	# 각 줄을 순서대로 타고
+	for 참가자번호 in 칸수.x:
+		var 현재줄번호 = 참가자번호
+		# 아래로 내려가면서 좌우로 이동
+		for y in 칸수.y:
+			if 사다리자료[현재줄번호][y].왼쪽연결길 == true:
+				# 왼쪽으로 한칸 이동
+				사다리자료[현재줄번호][y].왼쪽가는길 = 참가자번호
+				현재줄번호 = (현재줄번호-1 + 칸수.x) %칸수.x
+				참가자위치[참가자번호] = 현재줄번호
+				continue
+			if 사다리자료[(현재줄번호+1)%칸수.x][y].왼쪽연결길 == true:
+				# 오른쪽으로 한칸 이동
+				사다리자료[현재줄번호][y].오른쪽가는길 = 참가자번호
+				현재줄번호 = (현재줄번호+1) % 칸수.x
+				참가자위치[참가자번호] = 현재줄번호
+				continue
+
+
 var camera_move = false
 
 func _ready() -> void:
