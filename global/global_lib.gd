aa8caabde23794850a8b068ba0d2effeb7cbf084
SeukWon Kang <kasworld@gmail.com>
2025-02-22 14:02:36 +0900

    global lib

--- global/global_lib.gd
+++ global/global_lib.gd
@@ -69,3 +69,41 @@ func new_plane(size :Vector2, mat :Material)->MeshInstance3D:
 	var sp = MeshInstance3D.new()
 	sp.mesh = mesh
 	return sp
+
+
+func make_pos_by_rad_r_3d(rad:float, r :float, y :float =0)->Vector3:
+	return Vector3(sin(rad)*r, y, cos(rad)*r)
+
+func LineEdit만들기(t :String, co :Color) -> LineEdit:
+	var rtn = LineEdit.new()
+	rtn.text = t
+	rtn.alignment = HORIZONTAL_ALIGNMENT_CENTER
+	rtn.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	rtn.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	rtn.max_length = 10
+	rtn.add_theme_color_override("font_color", co)
+	rtn.add_theme_color_override("font_outline_color",Color.WHITE)
+	rtn.add_theme_constant_override("outline_size",1)
+	return rtn
+
+func Label3D만들기(t :String, co :Color) -> Label3D:
+	var rtn = Label3D.new()
+	rtn.text = t
+	rtn.modulate = co
+	rtn.pixel_size = 0.5
+	#rtn.no_depth_test = true
+	rtn.billboard = BaseMaterial3D.BILLBOARD_ENABLED
+	return rtn
+
+func 기둥만들기(h :float, r :float, co :Color)->MeshInstance3D:
+	var mat := StandardMaterial3D.new()
+	mat.albedo_color = co
+	var mesh = CylinderMesh.new()
+	mesh.height = h
+	mesh.bottom_radius = r
+	mesh.top_radius = r
+	mesh.radial_segments = 8 #clampi( int(r*4) , 64, 360)
+	mesh.material = mat
+	var sp = MeshInstance3D.new()
+	sp.mesh = mesh
+	return sp
--- main.gd
+++ main.gd
@@ -14,7 +14,7 @@ func 사다리칸수() -> Vector2i:
 func _ready() -> void:
 	var vp_size = get_viewport().get_visible_rect().size
 	var r = min(vp_size.x,vp_size.y)/2
-	RenderingServer.set_default_clear_color( Global3d.colors.default_clear)
+	RenderingServer.set_default_clear_color( GlobalLib.colors.default_clear)
 
 	$DirectionalLight3D.position = Vector3(r,r,r)
 	$DirectionalLight3D.look_at(Vector3.ZERO)
@@ -32,59 +32,22 @@ func _ready() -> void:
 	for i in 4:
 		참가자추가하기()
 
-func LineEdit만들기(t :String, co :Color) -> LineEdit:
-	var rtn = LineEdit.new()
-	rtn.text = t
-	rtn.alignment = HORIZONTAL_ALIGNMENT_CENTER
-	rtn.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	rtn.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	rtn.max_length = 10
-	rtn.add_theme_color_override("font_color", co)
-	rtn.add_theme_color_override("font_outline_color",Color.WHITE)
-	rtn.add_theme_constant_override("outline_size",1)
-	return rtn
-
-func Label3D만들기(t :String, co :Color) -> Label3D:
-	var rtn = Label3D.new()
-	rtn.text = t
-	rtn.modulate = co
-	rtn.pixel_size = 0.5
-	#rtn.no_depth_test = true
-	rtn.billboard = BaseMaterial3D.BILLBOARD_ENABLED
-	return rtn
-
-func 기둥만들기(h :float, r :float, co :Color)->MeshInstance3D:
-	var mat := StandardMaterial3D.new()
-	mat.albedo_color = co
-	var mesh = CylinderMesh.new()
-	mesh.height = h
-	mesh.bottom_radius = r
-	mesh.top_radius = r
-	mesh.radial_segments = 8 #clampi( int(r*4) , 64, 360)
-	mesh.material = mat
-	var sp = MeshInstance3D.new()
-	sp.mesh = mesh
-	return sp
-
 func 위치3D정리하기() -> void:
 	var n := $"사다리/세로기둥".get_child_count()
 	var r := 10 * n
 	for i in n:
 		var o = $"사다리/세로기둥".get_child(i)
-		o.position = make_pos_by_rad_r_3d(2*PI*i/n, r)
+		o.position = GlobalLib.make_pos_by_rad_r_3d(2*PI*i/n, r)
 		o.mesh.height = n * 30
 		o = $"사다리/출발목록".get_child(i)
-		o.position = make_pos_by_rad_r_3d(2*PI*i/n, r, n * 15 + 10)
+		o.position = GlobalLib.make_pos_by_rad_r_3d(2*PI*i/n, r, n * 15 + 10)
 		o = $"사다리/도착목록".get_child(i)
-		o.position = make_pos_by_rad_r_3d(2*PI*i/n, r, -n * 15 - 10)
-
-func make_pos_by_rad_r_3d(rad:float, r :float, y :float =0)->Vector3:
-	return Vector3(sin(rad)*r, y, cos(rad)*r)
+		o.position = GlobalLib.make_pos_by_rad_r_3d(2*PI*i/n, r, -n * 15 - 10)
 
 func 참가자추가하기() -> void:
 	var i = 참가자들.get_child_count()
 	참가자색.append(NamedColorList.color_list.pick_random()[0])
-	var 참가자 = LineEdit만들기("출발%d" % [i+1], 참가자색[i])
+	var 참가자 = GlobalLib.LineEdit만들기("출발%d" % [i+1], 참가자색[i])
 	참가자.text_changed.connect(
 		func(t :String):
 			참가자이름변경됨(i, t)
@@ -95,7 +58,7 @@ func 참가자추가하기() -> void:
 	)
 	참가자들.add_child(참가자)
 
-	var 도착점 = LineEdit만들기("도착%d" % [i+1], 참가자색[i])
+	var 도착점 = GlobalLib.LineEdit만들기("도착%d" % [i+1], 참가자색[i])
 	도착점.text_changed.connect(
 		func(t :String):
 			도착점이름변경됨(i, t)
@@ -105,11 +68,11 @@ func 참가자추가하기() -> void:
 			도착점.release_focus()
 	)
 	도착지점들.add_child(도착점)
-	var 기둥 := 기둥만들기(300, 5, 참가자색[i])
+	var 기둥 := GlobalLib.기둥만들기(300, 5, 참가자색[i])
 	$"사다리/세로기둥".add_child(기둥)
-	var 출발라벨 = Label3D만들기("출발%d" % [i+1], 참가자색[i])
+	var 출발라벨 = GlobalLib.Label3D만들기("출발%d" % [i+1], 참가자색[i])
 	$"사다리/출발목록".add_child(출발라벨)
-	var 도착라벨 = Label3D만들기("도착%d" % [i+1], 참가자색[i])
+	var 도착라벨 = GlobalLib.Label3D만들기("도착%d" % [i+1], 참가자색[i])
 	$"사다리/도착목록".add_child(도착라벨)
 
 	위치3D정리하기()
