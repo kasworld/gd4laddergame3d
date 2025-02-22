0370567f4c289b2c139d14bfb0243f56043a641e
SeukWon Kang <kasworld@gmail.com>
2025-02-22 14:40:44 +0900

    add settings

---
+++ global/settings.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const 최소칸수 = 3
+const 시작칸수 = 4
+const 최대칸수 = 30
--- main.gd
+++ main.gd
@@ -26,7 +26,7 @@ func _ready() -> void:
 	$오른쪽패널.position = Vector2(vp_size.x/2 + r, 0)
 	$"왼쪽패널/Scroll출발".get_v_scroll_bar().scrolling.connect(_on_참가자_scroll_scroll_started)
 	$"오른쪽패널/Scroll도착".get_v_scroll_bar().scrolling.connect(_on_도착지점_scroll_scroll_started)
-	for i in 4:
+	for i in Settings.시작칸수:
 		참가자추가하기()
 
 func 위치3D정리하기() -> void:
@@ -43,6 +43,8 @@ func 위치3D정리하기() -> void:
 
 func 참가자추가하기() -> void:
 	var i = 참가자들.get_child_count()
+	if i >= Settings.최대칸수:
+		return
 	참가자색.append(NamedColorList.color_list.pick_random()[0])
 	var 참가자 = GlobalLib.LineEdit만들기("출발%d" % [i+1], 참가자색[i])
 	참가자.text_changed.connect(
@@ -71,15 +73,14 @@ func 참가자추가하기() -> void:
 
 func 마지막참가자제거하기() -> void:
 	var 현재참가자수 = 참가자들.get_child_count()
-	if 현재참가자수 <= 3:
+	if 현재참가자수 <= Settings.최소칸수:
 		return
 	참가자색.pop_back()
-	var 마지막참가자 = 참가자들.get_child(현재참가자수-1)
-	참가자들.remove_child(마지막참가자)
-	var 마지막도착지 = 도착지점들.get_child(현재참가자수-1)
-	도착지점들.remove_child(마지막도착지)
-	var 마지막기둥 = $"사다리/세로기둥".get_child(현재참가자수-1)
-	$"사다리/세로기둥".remove_child(마지막기둥)
+	참가자들.remove_child(참가자들.get_child(현재참가자수-1))
+	도착지점들.remove_child(도착지점들.get_child(현재참가자수-1))
+	$"사다리/세로기둥".remove_child($"사다리/세로기둥".get_child(현재참가자수-1))
+	$"사다리/출발목록".remove_child($"사다리/출발목록".get_child(현재참가자수-1))
+	$"사다리/도착목록".remove_child($"사다리/도착목록".get_child(현재참가자수-1))
 	위치3D정리하기()
 
 func reset_camera_pos()->void:
